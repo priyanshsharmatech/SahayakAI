9fae2eea4a975abe4ac19dd4129b189f292f790f
Priyansh <priyansh3112004@gmail.com>
2026-04-28 19:28:48 +0530

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Support.ai",
+  title: "SahayakAI",
   description: "The chatbot that you can embed in your website",
 };
 
